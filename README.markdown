8ada217efc401a3c3ae7aacc8fa6fa6caa3985d4
rich <atkinsonr@gmail.com>
2011-04-09 22:49:31 +1000

    callbacks, multiple messages, sensible defaults

--- README.markdown
+++ README.markdown
@@ -1,5 +1,61 @@
 # jQuery Message Slider Plugin
 This plugin provides a cool way to temporarily display messages to the user.
 
-# Documentation
-[Message Slider docs and demo](http://projects.joreteg.com/jquery-sliding-message/)
+# Requires
+jQuery
+
+# License
+BSD
+
+# Examples
+
+# 1. Very basic usage, single message
+
+    <script type="text/javascript"> 
+    $(function() 
+    {
+        var message = 'Your changes have been saved!';
+        $.showMessage(message);
+    });
+    </script>
+ 
+# 2. Very basic usage, multiple messages
+
+    <script type="text/javascript"> 
+    $(function()
+    {
+        var messages = ['Your changes have been saved!', 'This message follows the first'];
+        $.showMessages(messages);      
+    });
+    </script>
+
+# 3. showMessage supports callbacks (but showMessages does not)
+
+    <script type="text/javascript"> 
+    $(function()
+    {
+        $.showMessage('Your changes have been saved!', {}, function()
+        {
+            console.log('Callback called after the message display has finished')
+        });
+    });
+    </script>
+
+# 4. These are all the options, showing their defaults.
+
+    <script type="text/javascript"> 
+    $(function()
+    {
+        $.showMessage('Your changes have been saved!',
+        {
+             id: 'sliding_message_box',
+             position: 'bottom',
+             size: '60',
+             backgroundColor: 'rgb(45, 45, 45)',
+             color: 'white',
+             delay: 3000,
+             speed: 500,
+             fontSize: '26px'
+        }, callback);
+    });
+    </script>
--- jquery.slidingmessage.js
+++ jquery.slidingmessage.js
@@ -1,14 +1,37 @@
+/*
+Copyright (c) 2011, Rich Atkinson - Distibuted under BSD License.
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+Based heavily on: http://projects.joreteg.com/jquery-sliding-message/ (BSD)
+
+With the following modifications:
+
+1. $.showMessage() now supports callbacks
+2. New function: $.showMessages(messages, options) displays a list of messages, sequentially.
+3. Some defaults changed and a new option 'color'.
+*/
+
 (function() {
-    jQuery.showMessage = function(message, options){
+    jQuery.showMessage = function(message, options, callback){
         // defaults
         settings = jQuery.extend({
              id: 'sliding_message_box',
              position: 'bottom',
-             size: '90',
-             backgroundColor: 'rgb(143, 177, 240)',
-             delay: 1500,
+             size: '60',
+             backgroundColor: 'rgb(45, 45, 45)',
+             color: 'white',
+             delay: 3000,
              speed: 500,
-             fontSize: '30px'
+             fontSize: '26px'
         }, options);        
         
         var elem = $('#' + settings.id);
@@ -20,6 +43,7 @@
             
             elem.css({'z-index': '999',
                       'background-color': settings.backgroundColor,
+                      'color': settings.color,
                       'text-align': 'center',
                       'position': 'absolute',
                       'position': 'fixed',
@@ -37,14 +61,34 @@
         if(settings.position == 'bottom'){
             elem.css('bottom', '-' + settings.size + 'px');
             elem.animate({bottom:'0'}, settings.speed);
-            delayed = '$("#' + settings.id + '").animate({bottom:"-' + settings.size + 'px"}, ' + settings.speed + ');';
+            delayed = function()
+            {
+                $("#"+settings.id).animate({bottom:"-"+settings.size+"px"}, settings.speed, callback);
+            }
             setTimeout(delayed, settings.delay);
         }
         else if(settings.position == 'top'){
             elem.css('top', '-' + settings.size + 'px');
             elem.animate({top:'0'}, settings.speed);
-            delayed = '$("#' + settings.id + '").animate({top:"-' + settings.size + 'px"}, ' + settings.speed + ');';
+            delayed = function()
+            {
+                $("#"+settings.id).animate({top:"-"+settings.size+"px"}, settings.speed, callback);
+            }
             setTimeout(delayed, settings.delay);
+        }
+    }
+})(jQuery);
+
+
+(function() {
+    jQuery.showMessages = function(messages, options)
+    {
+        if (messages && messages.length)
+        {
+            $.showMessage(messages.shift(), options, function()
+            {
+                $.showMessages(messages, options);
+            });
         }
     }
 })(jQuery);
